02136cebe25cabaca4becb710eba60d37b8c3525
escvir <escvir@hotmail.com>
2025-03-06 04:11:14 +0000

    Super_trunfo.c

--- Super_trunfo.c
+++ Super_trunfo.c
@@ -2,9 +2,13 @@
 
 int main() {
 
-int populacao, numerodospontosturisticos; 
+int populacao; 
+int numerodospontosturisticos;
 float area;
 float PIB;
+float densidadepopulacional;
+float PIBpercapita;
+float media;
 char estado;
 char cidade;
 char codigo;
@@ -15,11 +19,11 @@ char codigo;
     printf("Digite o nome da sua cidade: \n");
     scanf(" %s", &cidade);
 
-    printf("Digite o codigo da carta: %d\n");
+    printf("Digite o codigo da carta: \n");
     scanf("%s", &codigo);
 
     printf("Digite o numero  da populacao: \n");
-    scanf(" %e", &populacao);
+    scanf("%d", &populacao);
 
     printf("Digite o tamanho da area: \n");
     scanf(" %e", &area);
@@ -27,8 +31,28 @@ char codigo;
     printf("Digite o valor do PIB: \n");
     scanf(" %e", &PIB);
 
-    printf("Digite o numerodospontosturisticos: \n");
+    printf("Digite o numero dos pontos turisticos: \n");
     scanf(" %d", &numerodospontosturisticos);
+
+    printf("Digite o numero da populacao : \n");
+    scanf("%f", &populacao);
+
+    printf("Digite o tamanho da area: \n");
+    scanf("%f", &area);
+
+    media = (populacao / area);
+
+    printf("Densidade populacional: %.2f\n", media);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &PIB);
+
+    printf("Digite o numero da populacao: \n");
+    scanf("%f", &populacao);
+
+    media = (PIB / populacao);
+
+    printf("PIB per capita: %.2f\n", media);
 
     return 0;
 }
