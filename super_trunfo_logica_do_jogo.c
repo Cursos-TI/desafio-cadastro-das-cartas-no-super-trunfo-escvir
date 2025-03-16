0a8636ec1fe4223d27ebb26dbdc2913bff648677
escvir <escvir@hotmail.com>
2025-03-16 00:35:26 +0000

    super_trunfo_logica_do_jogo.c

--- super_trunfo_logica_do_jogo.c
+++ super_trunfo_logica_do_jogo.c
@@ -27,7 +27,7 @@ scanf("%s", &estado);
  scanf("%s", &codigo);
 
  printf("Carta 1, digite o numero  da populacao: \n");
- scanf("%ld", &populacao);
+ scanf("%d", &populacao);
 
  printf("Carta 1, digite o tamanho da area: \n");
  scanf(" %e", &area);
@@ -58,7 +58,7 @@ printf("Carta 2, digite o codigo da carta: \n");
 scanf("%s", &codigo);
 
 printf("Carta 2, digite o numero  da populacao: \n");
-scanf("%ld", &populacao);
+scanf("%d", &populacao);
 
 printf("Carta 2, digite o tamanho da area: \n");
 scanf(" %e", &area);
@@ -80,9 +80,9 @@ printf("O PIB per capita da carta 2 é: %.2f\n", media);
 
 //Aqui está sendo feito a comparação de um atributo das cartas
 if (populacao > populacao) {
-printf ("A populacao da carta1 é maior!\n");
+printf ("A carta1 venceu!\n");
 } else {
-printf ("A populacao da carta2 é maior!\n");
+printf ("A carta2 venceu!\n");
 }
 
 return 0;
